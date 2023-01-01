f326de09830424f816bd0aef4ea119f12c3bc85d
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-01-01 21:55:37 +0700

    Setup font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Source_Sans_Pro } from "@next/font/google";
+
+const ssp = Source_Sans_Pro({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${ssp.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
