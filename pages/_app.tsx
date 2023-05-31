edad48a207bec767ac25261268d5529c59a35d44
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-05-31 14:00:01 +0700

    Setup darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import "@splidejs/react-splide/css/core";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
+import "react-quill/dist/quill.snow.css";
 import { Provider } from "react-redux";
 import Layout from "~/layout";
 import { store } from "~/redux/store";
@@ -15,6 +17,42 @@ const ssp = Nunito({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (typeof window !== "undefined") {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }
+
+  useEffect(() => {
+    const handleChangeMode = () => {
+      console.log(localStorage.getItem("theme"));
+
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("storage", handleChangeMode);
+    }
+
+    return () => {
+      window.removeEventListener("storage", handleChangeMode);
+    };
+  });
+
   return (
     <>
       <style jsx global>{`
