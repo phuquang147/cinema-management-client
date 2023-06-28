beccd0f81262afd7fbb771b8627a42d362e3b021
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-06-28 20:14:42 +0700

    Add constants

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const GENDERS = [
+  { label: "Nam", value: "Nam" },
+  { label: "Nữ", value: "Nữ" },
+];
+
+export const SEAT_TYPES = {
+  SINGLE: "Ghế đơn",
+  DOUBLE: "Ghế đôi",
+  MAIN_DOUBLE: "Ghế đôi chính",
+  NONE: "Trống",
+};
