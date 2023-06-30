ec6cf384e0df3c591351fc0c5eefc1484e8e7d63
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-06-30 09:41:28 +0700

    Handle navitem click outside

--- layout/Header/NavItem.tsx
+++ layout/Header/NavItem.tsx
@@ -1,9 +1,10 @@
 import { IconChevronDown } from "@tabler/icons";
 import Link from "next/link";
-import React, { useState } from "react";
-import Divider from "~/components/UI/Divider";
-import { NavItemConfigProps } from "./navConfig";
 import { useRouter } from "next/router";
+import React, { useRef, useState } from "react";
+import Divider from "~/components/UI/Divider";
+import useClickOutside from "~/hooks/useClickOutside";
+import { NavItemConfigProps } from "./navConfig";
 
 type NavItemProps = {
   navItem: NavItemConfigProps;
@@ -13,10 +14,17 @@ type NavItemProps = {
 const NavItem: React.FC<NavItemProps> = ({ navItem, scrolled }) => {
   const { pathname } = useRouter();
   const [showChildren, setShowChildren] = useState<boolean>(false);
+  const subMenuRef = useRef<HTMLDivElement>(null);
+
+  const handleCloseChildren = () => {
+    setShowChildren(false);
+  };
 
   const handleToggleShowChildren = () => {
     setShowChildren((prev: boolean) => !prev);
   };
+
+  useClickOutside(subMenuRef, handleCloseChildren);
 
   const match = () => {
     const pathFirstPart = navItem.link?.split("/")[1];
@@ -35,6 +43,7 @@ const NavItem: React.FC<NavItemProps> = ({ navItem, scrolled }) => {
       </div>
       {showChildren && (
         <div
+          ref={subMenuRef}
           className={`absolute left-0 top-16 z-50 w-52 bg-black bg-opacity-70`}
         >
           <Divider />
@@ -44,6 +53,7 @@ const NavItem: React.FC<NavItemProps> = ({ navItem, scrolled }) => {
                 href={subNavItem.link}
                 key={subNavItem.name}
                 className="text-white p-2 hover:bg-gradient-to-r from-light-pink to-light-red rounded"
+                onClick={handleCloseChildren}
               >
                 {subNavItem.name}
               </Link>
