badd3785494e219eb5a62812db047fe5b1aa8169
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-07-15 13:12:56 +0700

    Update post detail ui

--- pages/bai-viet/[slug].tsx
+++ pages/bai-viet/[slug].tsx
@@ -4,7 +4,6 @@ import {
   NextPage,
 } from "next";
 import Head from "next/head";
-import NewPosts from "~/components/Posts/NewPosts";
 import QuickBooking from "~/components/QuickBooking";
 import IPost from "~/interfaces/post.interface";
 import PostServices from "~/services/postServices";
@@ -28,12 +27,15 @@ const Post: NextPage = ({
       <Head>
         <title>{post.title}</title>
       </Head>
-      <div className="bg-dark-bg-primary">
-        <div className="container mx-auto py-10 grid grid-cols-3 gap-4">
+      <div className="bg-light-bg-primary dark:bg-dark-bg-primary">
+        <div className="container mx-auto py-10 grid grid-cols-3 gap-10">
           <div className="col-span-3 lg:col-span-2">
             <h1 className="text-gray-text dark:text-light-text text-4xl mb-4">
               {post.title}
             </h1>
+            <p className="text-gray-text dark:text-light-text text-sm mb-2">
+              {`Tác giả: ${(post as IPost).author.name}`}
+            </p>
             <p className="text-gray-text dark:text-light-text text-sm mb-10">
               {`Ngày đăng: ${ISOToDateFormat((post as IPost).createdAt)} - ${
                 (post as IPost).view
@@ -45,7 +47,6 @@ const Post: NextPage = ({
                 __html: post.content,
               }}
             ></div>
-            {/* <NewPosts /> */}
           </div>
           <div className="col-span-0 lg:col-span-1">
             <QuickBooking />
